ac8b29faa6cbed937db4d16ed14c7ab90dfc8293
Optic the Wolf <65792938+Optic-the-Wolf@users.noreply.github.com>
2024-06-02 03:47:32 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -8,6 +8,12 @@ local query = get("message")
 local gpt35 = get("model1")
 local gpt4o = get("model2")
 local gpt4t = get("model3")
+local claudeinst = get("model4")
+local claude2 = get("model5")
+local claude3h = get("model6")
+local claude3s = get("model7")
+local llama3 = get("model8")
+local mixtral = get("model9")
 
 local model = "3.5"
 
@@ -27,6 +33,42 @@ gpt4t.on_click(function()
 	modelelem.set_content("GPT-4 Turbo")
 	modelelem2.set_content("GPT-4 Turbo")
 	model = "4t"
+end)
+
+claudeinst.on_click(function()
+	modelelem.set_content("Claude v1.2")
+	modelelem2.set_content("Claude v1.2")
+	model = "1.2"
+end)
+
+claude2.on_click(function()
+	modelelem.set_content("Claude v2.0")
+	modelelem2.set_content("Claude v2.0")
+	model = "2.0"
+end)
+
+claude3h.on_click(function()
+	modelelem.set_content("Claude 3 Haiku")
+	modelelem2.set_content("Claude 3 Haiku")
+	model = "3h"
+end)
+
+claude3s.on_click(function()
+	modelelem.set_content("Claude 3 Sonnet")
+	modelelem2.set_content("Claude 3 Sonnet")
+	model = "3s"
+end)
+
+llama3.on_click(function()
+	modelelem.set_content("LLaMA 3 70b")
+	modelelem2.set_content("LLaMA 3 70b")
+	model = "l3"
+end)
+
+mixtral.on_click(function()
+	modelelem.set_content("Mixtral 8x22b")
+	modelelem2.set_content("Mixtral 8x22b")
+	model = "m22"
 end)
 
 get("send").on_click(function()
